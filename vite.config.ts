17f6db307a12f9dad7cf06d908a2304fe7da92e5
techchrism <chris@techchrism.me>
2022-07-24 00:02:55 -0700

    use relative path for vite base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: './',
   server: {
     port: 3000,
   },
